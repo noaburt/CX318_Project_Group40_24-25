8c1c652e696725437a598b572d7123adb8db38f5
noaburt <noaburt051221@gmail/com>
2025-03-13 13:37:45 +0000

    Correct clock speed

--- frdmmcxn947_CX318_Project_Software/board/hardware_init.c
+++ frdmmcxn947_CX318_Project_Software/board/hardware_init.c
@@ -26,8 +26,8 @@ void BOARD_InitHardware(void)
     CLOCK_SetClkDiv(kCLOCK_DivTraceClk, 2U);
     CLOCK_AttachClk(kTRACE_DIV_to_TRACE);
 
-    /* attach FRO 12M to FLEXCOMM2 */
-    CLOCK_SetClkDiv(kCLOCK_DivFlexcom2Clk, 1u);
+    /* attach FRO 12M to FLEXCOMM2, divide to 400k */
+    CLOCK_SetClkDiv(kCLOCK_DivFlexcom2Clk, 30u);
     CLOCK_AttachClk(kFRO12M_to_FLEXCOMM2);
 
     BOARD_InitBootPins();
