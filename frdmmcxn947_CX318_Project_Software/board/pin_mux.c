7117e01ce97e07430ab8a40153ac18a1cedb3122
noaburt <noaburt051221@gmail/com>
2025-03-17 00:26:24 +0000

    Interrupt on either edge

--- frdmmcxn947_CX318_Project_Software/board/pin_mux.c
+++ frdmmcxn947_CX318_Project_Software/board/pin_mux.c
@@ -210,7 +210,7 @@ BOARD_TimerPins:
 - options: {callFromInitBoot: 'true', coreID: cm33_core0, enableClock: 'true'}
 - pin_list:
   - {pin_num: L4, peripheral: GPIO1, signal: 'GPIO, 22', pin_signal: PIO1_22/TRIG_IN3/FC5_P6/FC4_P2/CT_INP14/SCT0_OUT4/FLEXIO0_D30/SMARTDMA_PIO18/ADC1_A22, direction: INPUT,
-    gpio_per_interrupt_sel: output0, gpio_per_interrupt: kGPIO_InterruptLogicZero, pull_select: up, pull_enable: enable}
+    gpio_per_interrupt_sel: output0, gpio_per_interrupt: kGPIO_InterruptEitherEdge, pull_select: up, pull_enable: enable}
  * BE CAREFUL MODIFYING THIS COMMENT - IT IS YAML SETTINGS FOR TOOLS ***********
  */
 /* clang-format on */
@@ -242,8 +242,8 @@ void BOARD_TimerPins(void)
                       /* Interrupt Select: Interrupt, trigger output, or DMA request 0. */
                       | GPIO_ICR_IRQS(ICR_IRQS_irqs0));
 
-    /* Interrupt configuration on GPIO1_22 (pin L4): Interrupt when logic zero */
-    GPIO_SetPinInterruptConfig(BOARD_TIMERPINS_RESET_TIMER_GPIO, BOARD_TIMERPINS_RESET_TIMER_PIN, kGPIO_InterruptLogicZero);
+    /* Interrupt configuration on GPIO1_22 (pin L4): Interrupt on either edge */
+    GPIO_SetPinInterruptConfig(BOARD_TIMERPINS_RESET_TIMER_GPIO, BOARD_TIMERPINS_RESET_TIMER_PIN, kGPIO_InterruptEitherEdge);
 
     /* PORT1_22 (pin L4) is configured as PIO1_22 */
     PORT_SetPinMux(BOARD_TIMERPINS_RESET_TIMER_PORT, BOARD_TIMERPINS_RESET_TIMER_PIN, kPORT_MuxAlt0);
