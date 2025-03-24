e653cac034b05bcbc864e1ba760cdb8b744909d0
noaburt <noaburt051221@gmail/com>
2025-03-24 16:34:33 +0000

    Fixed drivers issue, ready to test

--- frdmmcxn947_CX318_Project_Software/Debug/drivers/subdir.mk
+++ frdmmcxn947_CX318_Project_Software/Debug/drivers/subdir.mk
@@ -8,6 +8,7 @@ C_SRCS += \
 ../drivers/fsl_common.c \
 ../drivers/fsl_common_arm.c \
 ../drivers/fsl_gpio.c \
+../drivers/fsl_inputmux.c \
 ../drivers/fsl_lpflexcomm.c \
 ../drivers/fsl_lpi2c.c \
 ../drivers/fsl_lpuart.c \
@@ -20,6 +21,7 @@ C_DEPS += \
 ./drivers/fsl_common.d \
 ./drivers/fsl_common_arm.d \
 ./drivers/fsl_gpio.d \
+./drivers/fsl_inputmux.d \
 ./drivers/fsl_lpflexcomm.d \
 ./drivers/fsl_lpi2c.d \
 ./drivers/fsl_lpuart.d \
@@ -32,6 +34,7 @@ OBJS += \
 ./drivers/fsl_common.o \
 ./drivers/fsl_common_arm.o \
 ./drivers/fsl_gpio.o \
+./drivers/fsl_inputmux.o \
 ./drivers/fsl_lpflexcomm.o \
 ./drivers/fsl_lpi2c.o \
 ./drivers/fsl_lpuart.o \
@@ -52,7 +55,7 @@ drivers/%.o: ../drivers/%.c drivers/subdir.mk
 clean: clean-drivers
 
 clean-drivers:
-	-$(RM) ./drivers/fsl_clock.d ./drivers/fsl_clock.o ./drivers/fsl_common.d ./drivers/fsl_common.o ./drivers/fsl_common_arm.d ./drivers/fsl_common_arm.o ./drivers/fsl_gpio.d ./drivers/fsl_gpio.o ./drivers/fsl_lpflexcomm.d ./drivers/fsl_lpflexcomm.o ./drivers/fsl_lpi2c.d ./drivers/fsl_lpi2c.o ./drivers/fsl_lpuart.d ./drivers/fsl_lpuart.o ./drivers/fsl_reset.d ./drivers/fsl_reset.o ./drivers/fsl_sctimer.d ./drivers/fsl_sctimer.o ./drivers/fsl_spc.d ./drivers/fsl_spc.o
+	-$(RM) ./drivers/fsl_clock.d ./drivers/fsl_clock.o ./drivers/fsl_common.d ./drivers/fsl_common.o ./drivers/fsl_common_arm.d ./drivers/fsl_common_arm.o ./drivers/fsl_gpio.d ./drivers/fsl_gpio.o ./drivers/fsl_inputmux.d ./drivers/fsl_inputmux.o ./drivers/fsl_lpflexcomm.d ./drivers/fsl_lpflexcomm.o ./drivers/fsl_lpi2c.d ./drivers/fsl_lpi2c.o ./drivers/fsl_lpuart.d ./drivers/fsl_lpuart.o ./drivers/fsl_reset.d ./drivers/fsl_reset.o ./drivers/fsl_sctimer.d ./drivers/fsl_sctimer.o ./drivers/fsl_spc.d ./drivers/fsl_spc.o
 
 .PHONY: clean-drivers
 
